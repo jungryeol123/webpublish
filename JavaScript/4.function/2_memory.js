268f4118bb7f091a9f3b878fbaaa63d9ad487d4b
yoon <46823971@naver.com>
2025-08-20 18:18:19 +0900

    modified commit

--- JavaScript/4.function/2_memory.js
+++ JavaScript/4.function/2_memory.js
@@ -4,8 +4,8 @@
 // 문자 --> 숫자 변환 함수
 import { toNumber } from "../commons/util.js";
 
-add(100,200);
-add(100,'200');
+// add(100,200);
+// add(100,'200');
 
 // function add(num1,num2) {
 //     if (typeof (num1 && num2) === 'string') {
@@ -34,19 +34,19 @@ add(100,'200');
 //     }
 // }
 
-min(200,100);
-min('200',100);
-min(200,100);
-min('300',100);
+// min(200,100);
+// min('200',100);
+// min(200,100);
+// min('300',100);
 
-const min = (num1, num2) => {
-    let obj = toNumber(num1, num2);
-    if(obj.num1 > obj.num2) {
-        console.log(`min = ${obj.num1 - obj.num2}`);        
-    } else {
-        console.log(`값이 올바르지 않습니다.~`);                
-    }
-}
+// const min = (num1, num2) => {
+//     let obj = toNumber(num1, num2);
+//     if(obj.num1 > obj.num2) {
+//         console.log(`min = ${obj.num1 - obj.num2}`);        
+//     } else {
+//         console.log(`값이 올바르지 않습니다.~`);                
+//     }
+// }
 
 
 // function toNumber(num1, num2) { 
@@ -66,3 +66,15 @@ const min = (num1, num2) => {
 // } 
 // min(200,100); 
 // min('200',100);
+
+function min2 (num1, num2) {
+    let obj = undefined;
+    if(num1 > num2) {
+        if((typeof num1 === 'string') || (typeof num2 === 'string')) {
+            obj = toNumber(num1,num2);
+            console.log(obj.num1 - obj.num2);
+        } else {
+            console.log(num1 - num2);
+        }
+    }
+}
