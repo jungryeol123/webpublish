1f6e7413e08ddf00de4543681030d410026ffbe4
yoon <46823971@naver.com>
2025-09-10 16:12:39 +0900

    modified commit

---
+++ react/basic-app/src/AppProduct.js
@@ -0,0 +1,11 @@
+import { BestProduct, BestProductImage } from "./components/Product.jsx";
+import './css/Product.css'
+
+export default function App() {
+    
+    return(
+        <>
+            <BestProduct />
+        </>
+    );
+}
--- react/basic-app/src/index.js
+++ react/basic-app/src/index.js
@@ -5,7 +5,8 @@ import reportWebVitals from './reportWebVitals.js';
 // import App from './App.js';
 // import App from './AppAvatar.js';
 // import App from './AppButton.js';
-import App from './AppCounter.js';
+// import App from './AppCounter.js';
+import App from './AppProduct.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
